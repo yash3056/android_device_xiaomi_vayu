64732af83f6ae6675e8a02d47f4df78804231aeb
yashlearnpython <73551736+yashlearnpython@users.noreply.github.com>
2021-06-26 20:38:16 +0530

    vayu: bringup for lineage
    thanx pro guy for reference https://github.com/xiaomi-sm8150-devs/android_device_xiaomi_vayu
    
    not blame as a kanger if u feel here u need credit but i am just newbie to use github so i not know much about it

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2018-2021 ArrowOS
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_vayu.mk
+    $(LOCAL_DIR)/lineage_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_vayu-eng \
-    arrow_vayu-userdebug \
-    arrow_vayu-user
+    lineage_vayu-user \
+    lineage_vayu-userdebug \
+    lineage_vayu-eng
